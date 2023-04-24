df1714499fed1fd168bc47919246be5467a369f2
Sam Bentley <sam.bentley@contino.io>
2023-04-24 11:45:53 +1000

    add another bucket

--- test.tf
+++ test.tf
@@ -5,3 +5,11 @@ resource "google_storage_bucket" "auto-expire" {
 
   public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket" "test2" {
+  name          = "no-public-access-bucket222"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
